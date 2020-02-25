ec0ed395b5ac54f06ccaf2129be52d9332e15dcf
Chung Thế Hào <chungthehao@gmail.com>
2020-02-25 23:14:15 +0700

    Apollo Container

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http'; // let us connect our client to our "/graphql" endpoint
 import { InMemoryCache } from 'apollo-cache-inmemory'; // Apollo uses to cache the data that it fetches
-import { ApolloClient, gql } from 'apollo-boost';
+import { ApolloClient } from 'apollo-boost';
 
 import { store, persistor } from './redux/store';
 
@@ -26,24 +26,6 @@ const client = new ApolloClient({
   link: httpLink,
   cache
 });
-
-// query trả về 1 promise
-client.query({
-  query: gql`
-    {
-      getCollectionsByTitle(title: "hats") {
-        id
-        title
-        items {
-          id
-          name
-          price
-          imageUrl
-        }
-      }
-    }
-  `
-}).then(res => console.log(res));
 
 ReactDOM.render(
   <ApolloProvider client={client}>
